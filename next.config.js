2e687251a33c1f5fa0118d3182a1df9f9f0ba48a
xvca <dev@luca.vin>
2025-11-26 17:27:22 +1100

    Updated dependencies
    - updated to next.js 16
    - removed next-pwa as outdated and upated next config accordingly
    - updated dockerfile and nvmrc to use node 22

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-	dest: 'public',
-	register: true,
-	skipWaiting: true,
-})
-
-module.exports = withPWA({
+const nextConfig = {
 	reactStrictMode: true,
-})
+}
+
+module.exports = nextConfig
